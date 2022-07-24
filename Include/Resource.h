8dfbf9e39d5bdc79d0fd34622c43139e803a5b3d
TOMOQ1024 <tomo_suzu_16384@au.com>
2022-07-24 14:19:44 +0900

    [add]コントロール背景の実装，メニューの削除

--- Include/Resource.h
+++ Include/Resource.h
@@ -4,18 +4,10 @@
 
 #define IDS_APP_TITLE			103
 
-#define IDR_MAINFRAME			128
-#define IDD_GRAPHME_DIALOG	102
-#define IDD_ABOUTBOX			103
-#define IDM_ABOUT				104
-#define IDM_EXIT				105
 #define IDI_GRAPHME			107
 #define IDI_SMALL				108
 #define IDC_GRAPHME			109
 #define IDC_MYICON				2
-#ifndef IDC_STATIC
-#define IDC_STATIC				-1
-#endif
 // 新しいオブジェクトの次の既定値
 //
 #ifdef APSTUDIO_INVOKED
--- Source/Graph Me.cpp
+++ Source/Graph Me.cpp
@@ -3,6 +3,7 @@
 
 #include "framework.h"
 #include "Graph Me.h"
+#include <math.h>
 
 #define MAX_LOADSTRING 100
 
@@ -10,6 +11,14 @@
 HINSTANCE hInst;                                // 現在のインターフェイス
 WCHAR szTitle[MAX_LOADSTRING];                  // タイトル バーのテキスト
 WCHAR szWindowClass[MAX_LOADSTRING];            // メイン ウィンドウ クラス名
+
+// カーソルの描画(没)
+// http://nagoyacoder.web.fc2.com/win32api/mcursor.html
+//INT cur_width;
+//INT cur_height;
+//INT cur_size;
+//char* cur_and;
+//char* cur_xor;
 
 // このコード モジュールに含まれる関数の宣言を転送します:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
@@ -75,7 +84,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.hInstance      = hInstance;
     wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_GRAPHME));
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.hbrBackground  = CreateSolidBrush(BLACK_BRUSH);
     wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_GRAPHME);
     wcex.lpszClassName  = szWindowClass;
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
@@ -123,34 +132,114 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+    static UINT width = 0, height = 0, ctrl_width = 250;
+    static INT mx, my;
+    static BOOL flg0 = FALSE;
+    static BOOL flg1 = FALSE;
+    static HCURSOR hCursor = NULL;
+
     switch (message)
     {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // 選択されたメニューの解析:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
+    case WM_CREATE:
+    {
+        //int i;
+        //
+        //cur_width = GetSystemMetrics(SM_CXCURSOR);
+        //cur_height = GetSystemMetrics(SM_CYCURSOR);
+        //cur_size = cur_width / 8 * cur_height;
+        //cur_and = (char*)malloc(cur_size);
+        //cur_xor = (char*)malloc(cur_size);
+        // 
+        //for (i = 0; i < cur_size; i++) {
+        //    if (i % (cur_width / 8) < (cur_width / 16)) {
+        //        cur_and[i] = 0x00;
+        //    }
+        //    else {
+        //        cur_and[i] = 0xFF;
+        //    }
+        //    if (i < (cur_size / 2)) {
+        //        cur_xor[i] = 0x00;
+        //    }
+        //    else {
+        //        cur_xor[i] = 0xFF;
+        //    }
+        //}
+        //
+        //SetCursor(NULL);
+        //DestroyCursor(hCursor);
+        //hCursor = CreateCursor(hInst, cur_width / 2, cur_height / 2, cur_width, cur_height, cur_and, cur_xor);
+        //SetCursor(hCursor);
+    }
+    case WM_SIZE:
+    {
+        width = LOWORD(lParam);
+        height = HIWORD(lParam);
         break;
+    }
     case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: HDC を使用する描画コードをここに追加してください...
-            EndPaint(hWnd, &ps);
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+
+        
+
+        // コントロールの描画
+        SelectObject(hdc, CreateSolidBrush(RGB(0xFF, 0xFF, 0xFF)));
+        Rectangle(hdc, 0, 0, ctrl_width, height);
+        SelectObject(hdc, CreateSolidBrush(RGB(0x00, 0x00, 0x00)));
+        Rectangle(hdc, ctrl_width, 0, width, height);
+
+        TCHAR str[128];
+        wsprintf(str, L"width: %d, height: %d", width, height);
+        TextOut(hdc, 10, 10, str, lstrlen(str));
+        wsprintf(str, L"mouseX: %d, mouseY: %d", mx, my);
+        TextOut(hdc, 10, 30, str, lstrlen(str));
+        wsprintf(str, L"cwidth: %d", ctrl_width);
+        TextOut(hdc, 10, 50, str, lstrlen(str));
+        wsprintf(str, L"%s", flg0 ? L"TRUE" : L"FALSE");
+        TextOut(hdc, 10, 70, str, lstrlen(str));
+        wsprintf(str, L"%s", flg1 ? L"TRUE" : L"FALSE");
+        TextOut(hdc, 10, 90, str, lstrlen(str));
+
+        EndPaint(hWnd, &ps);
+    }
+    break;
+    case WM_LBUTTONDOWN:
+    {
+        flg1 = FALSE;
+        if (flg0) {
+            flg1 = TRUE;
+            InvalidateRect(hWnd, NULL, FALSE);
         }
         break;
+    }
+    case WM_LBUTTONUP:
+    {
+        flg1 = FALSE;
+    }
+    case WM_MOUSEMOVE:
+    {
+        POINTS mousePos = MAKEPOINTS(lParam);
+        mx = mousePos.x;
+        my = mousePos.y;
+        if (flg1) {
+            ctrl_width = mx;
+            SetCursor(LoadCursor(NULL, IDC_SIZEWE));
+        }
+        else if (abs((LONG)(mx - ctrl_width)) < 3) {
+            flg0 = TRUE;
+            SetCursor(LoadCursor(NULL, IDC_SIZEWE));
+        }
+        else {
+            flg0 = FALSE;
+        }
+        InvalidateRect(hWnd, NULL, FALSE);
+        break;
+    }
     case WM_DESTROY:
+        //free(cur_and);
+        //free(cur_xor);
+        //DestroyCursor(hCursor);
         PostQuitMessage(0);
         break;
     default:
