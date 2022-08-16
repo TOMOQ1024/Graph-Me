a2b57dd2b03f8622b4f033547604c2e2fd8fa9c2
TOMOQ1024 <tomo_suzu_16384@au.com>
2022-08-17 02:29:34 +0900

    [add] タイトルシーンの右画面描画 for issue #17

--- Include/Draw.h
+++ Include/Draw.h
@@ -8,9 +8,17 @@ void Draw(HDC hdc, HDC hMemDC, INT mx, INT my);
 // コントロールの描画
 void DrawCtrl(HDC hdc, HDC hMemDC, INT mx, INT my);
 
-// 軸,グラフ,数式の描画
+// 線分
 void Segment(HDC hdc, double x0, double y0, double x1, double y1);
-void tSegment(HDC hdc, INT x0, INT y0, INT X0, INT Y0, INT X1, INT Y1);
+// テキスト用セグメント
+void tSegment(HDC hdc, double x0, double y0, double X0, double Y0, double X1, double Y1);
+// アニメーション付き線分
+void mtSegment(
+	HDC hdc, double t, double x0, double y0, INT xf0, INT yf0, INT xt0, INT yt0,
+	INT xf1, INT yf1, INT xt1, INT yt1, INT xf2, INT yf2, INT xt2, INT yt2
+);
+
+// 軸,グラフ,数式の描画
 void DrawMain(HDC hdc, HDC hMemDC);
 void DrawAxis(HDC hdc, HDC hMemDC);
 void DrawGraph(HDC hdc, HDC hMemDC);
--- Source/DrawMain.c
+++ Source/DrawMain.c
@@ -10,18 +10,30 @@
 
 void Segment(HDC hdc, double x0, double y0, double x1, double y1)
 {
-	INT X0 = pane.lWidth + pane.paddingX + (graph.x0 + x0 * graph.scale) * pane.radius * 2;
-	INT Y0 = pane.paddingY + (graph.y0 - y0 * graph.scale) * pane.radius * 2;
-	INT X1 = pane.lWidth + pane.paddingX + (graph.x0 + x1 * graph.scale) * pane.radius * 2;
-	INT Y1 = pane.paddingY + (graph.y0 - y1 * graph.scale) * pane.radius * 2;
+	INT X0 = (INT)(pane.lWidth + pane.paddingX + (graph.x0 + x0 * graph.scale) * pane.radius * 2);
+	INT Y0 = (INT)(pane.paddingY + (graph.y0 - y0 * graph.scale) * pane.radius * 2);
+	INT X1 = (INT)(pane.lWidth + pane.paddingX + (graph.x0 + x1 * graph.scale) * pane.radius * 2);
+	INT Y1 = (INT)(pane.paddingY + (graph.y0 - y1 * graph.scale) * pane.radius * 2);
 
 	MoveToEx(hdc, X0, Y0, NULL);
 	LineTo(hdc, X1, Y1);
 }
 
-void tSegment(HDC hdc, INT x0, INT y0, INT X0, INT Y0, INT X1, INT Y1)
+void tSegment(HDC hdc, double x0, double y0, double X0, double Y0, double X1, double Y1)
 {
 	Segment(hdc, x0 + (X0 * 0.3 + 0.2), y0 + Y0, x0 + (X1 * 0.3 + 0.2), y0 + Y1);
+}
+
+void mtSegment(
+	HDC hdc, double t, double x0,  double y0, INT xf0, INT yf0, INT xt0, INT yt0,
+	INT xf1, INT yf1, INT xt1, INT yt1, INT xf2, INT yf2, INT xt2, INT yt2
+)
+{
+	tSegment(
+		hdc, x0, y0,
+		Ease(t, xf0, xf1, xf2), Ease(t, yf0, yf1, yf2),
+		Ease(t, xt0, xt1, xt2), Ease(t, yt0, yt1, yt2)
+	);
 }
 
 
@@ -41,50 +53,50 @@ void DrawAxis(HDC hdc, HDC hMemDC)
 
 	// y é≤ éÂê¸
 	for (
-		int x0 = -(pane.paddingX + pane.radius * 2 * graph.x0) / (pane.radius * 2 * graph.scale);
+		int x0 = (INT)(-(pane.paddingX + pane.radius * 2 * graph.x0) / (pane.radius * 2 * graph.scale));
 		x0 <= (pane.paddingX + pane.radius * 2 * (1 - graph.x0)) / (pane.radius * 2 * graph.scale);
 		x0++
 	) {
 		x = pane.lWidth + pane.paddingX + pane.radius * 2 * (graph.x0 + graph.scale * x0);
 		DeleteObject(SelectObject(hMemDC, CreatePen(PS_SOLID, 1, 0x404040)));
-		MoveToEx(hMemDC, x, 0, NULL);
-		LineTo(hMemDC, x, pane.height);
+		MoveToEx(hMemDC, (INT)x, 0, NULL);
+		LineTo(hMemDC, (INT)x, pane.height);
 	}
 
 	// x é≤ éÂê¸
 	for (
-		int y0 = -(pane.paddingY + pane.radius * 2 * graph.y0) / (pane.radius * 2 * graph.scale);
+		int y0 = (INT)(-(pane.paddingY + pane.radius * 2 * graph.y0) / (pane.radius * 2 * graph.scale));
 		y0 <= (pane.paddingY + pane.radius * 2 * (1 - graph.y0)) / (pane.radius * 2 * graph.scale);
 		y0++
 		) {
 		y = pane.paddingY + pane.radius * 2 * (graph.y0 + graph.scale * y0);
 		DeleteObject(SelectObject(hMemDC, CreatePen(PS_SOLID, 1, 0x404040)));
-		MoveToEx(hMemDC, pane.lWidth, y, NULL);
-		LineTo(hMemDC, pane.width, y);
+		MoveToEx(hMemDC, pane.lWidth, (INT)y, NULL);
+		LineTo(hMemDC, pane.width, (INT)y);
 	}
 
 	// y é≤ ïõê¸
 	for (
-		int x0 = -(pane.paddingX + pane.radius * 2 * graph.x0) / (pane.radius * 2 * graph.scale * 5);
+		int x0 = (INT)(-(pane.paddingX + pane.radius * 2 * graph.x0) / (pane.radius * 2 * graph.scale * 5));
 		x0 <= (pane.paddingX + pane.radius * 2 * (1 - graph.x0)) / (pane.radius * 2 * graph.scale * 5);
 		x0++
 		) {
 		x = pane.lWidth + pane.paddingX + pane.radius * 2 * (graph.x0 + graph.scale * 5 * x0);
 		DeleteObject(SelectObject(hMemDC, CreatePen(PS_SOLID, x0 ? 1 : 3, 0x808080)));
-		MoveToEx(hMemDC, x, 0, NULL);
-		LineTo(hMemDC, x, pane.height);
+		MoveToEx(hMemDC, (INT)x, 0, NULL);
+		LineTo(hMemDC, (INT)x, pane.height);
 	}
 
 	// x é≤ ïõê¸
 	for (
-		int y0 = -(pane.paddingY + pane.radius * 2 * graph.y0) / (pane.radius * 2 * graph.scale * 5);
+		int y0 = (INT)(-(pane.paddingY + pane.radius * 2 * graph.y0) / (pane.radius * 2 * graph.scale * 5));
 		y0 <= (pane.paddingY + pane.radius * 2 * (1 - graph.y0)) / (pane.radius * 2 * graph.scale * 5);
 		y0++
 		) {
 		y = pane.paddingY + pane.radius * 2 * (graph.y0 + graph.scale * 5 * y0);
 		DeleteObject(SelectObject(hMemDC, CreatePen(PS_SOLID, y0 ? 1 : 3, 0x808080)));
-		MoveToEx(hMemDC, pane.lWidth, y, NULL);
-		LineTo(hMemDC, pane.width, y);
+		MoveToEx(hMemDC, pane.lWidth, (INT)y, NULL);
+		LineTo(hMemDC, pane.width, (INT)y);
 	}
 
 	DeleteObject(SelectObject(hMemDC, GetStockObject(BLACK_PEN)));
@@ -99,53 +111,288 @@ void DrawGraph(HDC hdc, HDC hMemDC)
 		double t = sliders[2].value;
 		DeleteObject(SelectObject(hMemDC, CreatePen(PS_SOLID, 4, 0x00FFFF)));
 
-		// G
-		tSegment(hMemDC, -4, 2, 2, 2, 0, 2);
-		tSegment(hMemDC, -4, 2, 0, 0, 0, 2);
-		tSegment(hMemDC, -4, 2, 0, 0, 2, 0);
-		tSegment(hMemDC, -4, 2, 2, 1, 2, 0);
-		tSegment(hMemDC, -4, 2, 2, 1, 1, 1);
+		/*  å≈íËïî  */ {
+			// G
+			tSegment(hMemDC, -4, 1.5, 2, 2, 0, 2);
+			tSegment(hMemDC, -4, 1.5, 0, 0, 0, 2);
+			tSegment(hMemDC, -4, 1.5, 0, 0, 2, 0);
+			tSegment(hMemDC, -4, 1.5, 2, 1, 2, 0);
+			tSegment(hMemDC, -4, 1.5, 2, 1, 1, 1);
 
-		// R
-		tSegment(hMemDC, -3, 2, 0, 0, 0, 2);
-		tSegment(hMemDC, -3, 2, 2, 2, 0, 2);
-		tSegment(hMemDC, -3, 2, 2, 2, 2, 1);
-		tSegment(hMemDC, -3, 2, 0, 1, 2, 1);
-		tSegment(hMemDC, -3, 2, 1, 1, 2, 0);
+			// R
+			tSegment(hMemDC, -3, 1.5, 0, 0, 0, 2);
+			tSegment(hMemDC, -3, 1.5, 2, 2, 0, 2);
+			tSegment(hMemDC, -3, 1.5, 2, 2, 2, 1);
+			tSegment(hMemDC, -3, 1.5, 0, 1, 2, 1);
+			tSegment(hMemDC, -3, 1.5, 1, 1, 2, 0);
 
-		// A
-		tSegment(hMemDC, -2, 2, 0, 0, 0, 2);
-		tSegment(hMemDC, -2, 2, 2, 2, 0, 2);
-		tSegment(hMemDC, -2, 2, 2, 2, 2, 0);
-		tSegment(hMemDC, -2, 2, 0, 1, 2, 1);
+			// A
+			tSegment(hMemDC, -2, 1.5, 0, 0, 0, 2);
+			tSegment(hMemDC, -2, 1.5, 2, 2, 0, 2);
+			tSegment(hMemDC, -2, 1.5, 2, 2, 2, 0);
+			tSegment(hMemDC, -2, 1.5, 0, 1, 2, 1);
 
-		// R
-		tSegment(hMemDC, -1, 2, 0, 0, 0, 2);
-		tSegment(hMemDC, -1, 2, 2, 2, 0, 2);
-		tSegment(hMemDC, -1, 2, 2, 2, 2, 1);
-		tSegment(hMemDC, -1, 2, 0, 1, 2, 1);
+			// P
+			tSegment(hMemDC, -1, 1.5, 0, 0, 0, 2);
+			tSegment(hMemDC, -1, 1.5, 2, 2, 0, 2);
+			tSegment(hMemDC, -1, 1.5, 2, 2, 2, 1);
+			tSegment(hMemDC, -1, 1.5, 0, 1, 2, 1);
 
-		// H
-		tSegment(hMemDC, 0, 2, 0, 0, 0, 2);
-		tSegment(hMemDC, 0, 2, 2, 0, 2, 2);
-		tSegment(hMemDC, 0, 2, 0, 1, 2, 1);
+			// H
+			tSegment(hMemDC, 0, 1.5, 0, 0, 0, 2);
+			tSegment(hMemDC, 0, 1.5, 2, 0, 2, 2);
+			tSegment(hMemDC, 0, 1.5, 0, 1, 2, 1);
 
-		// _
-		tSegment(hMemDC, 1, 2, 0, 0, 2, 0);
+			// _
+			tSegment(hMemDC, 1, 1.5, 0, 0, 2, 0);
 
-		// M
-		tSegment(hMemDC, 2, 2, 0, 0, 0, 2);
-		tSegment(hMemDC, 2, 2, 2, 2, 0, 2);
-		tSegment(hMemDC, 2, 2, 2, 2, 2, 0);
-		tSegment(hMemDC, 2, 2, 1, 0, 1, 2);
+			// M
+			tSegment(hMemDC, 2, 1.5, 0, 0, 0, 2);
+			tSegment(hMemDC, 2, 1.5, 2, 2, 0, 2);
+			tSegment(hMemDC, 2, 1.5, 2, 2, 2, 0);
+			tSegment(hMemDC, 2, 1.5, 1, 0, 1, 2);
 
-		// G
-		tSegment(hMemDC, 3, 2, 2, 2, 0, 2);
-		tSegment(hMemDC, 3, 2, 0, 0, 0, 2);
-		tSegment(hMemDC, 3, 2, 0, 0, 2, 0);
-		tSegment(hMemDC, 3, 2, 0, 1, 2, 1);
+			// E
+			tSegment(hMemDC, 3, 1.5, 2, 2, 0, 2);
+			tSegment(hMemDC, 3, 1.5, 0, 0, 0, 2);
+			tSegment(hMemDC, 3, 1.5, 0, 0, 2, 0);
+			tSegment(hMemDC, 3, 1.5, 0, 1, 2, 1);
+		}
 
-		Segment(hMemDC, 0, -2, Ease(t, -1, 2, 4), -3);
+		/*  â¬ìÆïî  */ {
+			/*  _S_  */ {
+				mtSegment(
+					hMemDC, t, -4, -3.5,
+					0, 0, 2, 0,
+					0, 2, 2, 2,
+					0, 0, 2, 0
+				);
+				mtSegment(
+					hMemDC, t, -4, -3.5,
+					2, 0, 0, 0,
+					0, 1, 2, 1,
+					0, 0, 2, 0
+				);
+				mtSegment(
+					hMemDC, t, -4, -3.5,
+					0, 0, 2, 0,
+					0, 0, 2, 0,
+					0, 0, 2, 0
+				);
+				mtSegment(
+					hMemDC, t, -4, -3.5,
+					2, 0, 2, 0,
+					0, 1, 0, 2,
+					0, 0, 0, 0
+				);
+				mtSegment(
+					hMemDC, t, -4, -3.5,
+					0, 0, 0, 0,
+					2, 1, 2, 0,
+					2, 0, 2, 0
+				);
+			}
+			/*  _E_  */ {
+				mtSegment(
+					hMemDC, t, -3, -3.5,
+					0, 0, 2, 0,
+					0, 2, 2, 2,
+					0, 0, 2, 0
+				);
+				mtSegment(
+					hMemDC, t, -3, -3.5,
+					2, 0, 2, 0,
+					0, 1, 2, 1,
+					2, 0, 2, 0
+				);
+				mtSegment(
+					hMemDC, t, -3, -3.5,
+					0, 0, 2, 0,
+					0, 0, 2, 0,
+					0, 0, 2, 0
+				);
+				mtSegment(
+					hMemDC, t, -3, -3.5,
+					2, 0, 2, 0,
+					0, 1, 0, 2,
+					2, 0, 2, 0
+				);
+				mtSegment(
+					hMemDC, t, -3, -3.5,
+					0, 0, 0, 0,
+					0, 1, 0, 0,
+					2, 0, 0, 0
+				);
+			}
+			/*  ETP  */ {
+				mtSegment(
+					hMemDC, t, -2, -3.5,
+					0, 2, 2, 2,
+					0, 2, 2, 2,
+					0, 2, 2, 2
+				);
+				mtSegment(
+					hMemDC, t, -2, -3.5,
+					0, 1, 2, 1,
+					1, 1, 1, 1,
+					2, 1, 2, 2
+				);
+				mtSegment(
+					hMemDC, t, -2, -3.5,
+					0, 0, 2, 0,
+					1, 0, 1, 0,
+					0, 1, 2, 1
+				);
+				mtSegment(
+					hMemDC, t, -2, -3.5,
+					0, 0, 0, 2,
+					1, 0, 1, 2,
+					0, 0, 0, 2
+				);
+			}
+			/*  XTL  */ {
+				mtSegment(
+					hMemDC, t, -1, -3.5,
+					0, 0, 2, 2,
+					1, 0, 1, 2,
+					0, 0, 0, 2
+				);
+				mtSegment(
+					hMemDC, t, -1, -3.5,
+					0, 2, 2, 0,
+					0, 2, 2, 2,
+					0, 0, 2, 0
+				);
+				mtSegment(
+					hMemDC, t, -1, -3.5,
+					0, 2, 2, 0,
+					0, 2, 2, 2,
+					0, 0, 2, 0
+				);
+			}
+			/*  IIA  */ {
+				mtSegment(
+					hMemDC, t, 0, -3.5,
+					0, 0, 2, 0,
+					0, 0, 2, 0,
+					0, 2, 2, 2
+				);
+				mtSegment(
+					hMemDC, t, 0, -3.5,
+					0, 2, 2, 2,
+					0, 2, 2, 2,
+					0, 1, 2, 1
+				);
+				mtSegment(
+					hMemDC, t, 0, -3.5,
+					1, 0, 1, 2,
+					1, 0, 1, 2,
+					0, 0, 0, 2
+				);
+				mtSegment(
+					hMemDC, t, 0, -3.5,
+					1, 0, 1, 2,
+					1, 0, 1, 2,
+					2, 0, 2, 2
+				);
+			}
+			/*  TNY  */ {
+				mtSegment(
+					hMemDC, t, 1, -3.5,
+					1, 0, 1, 2,
+					0, 0, 0, 2,
+					0, 1, 0, 2
+				);
+				mtSegment(
+					hMemDC, t, 1, -3.5,
+					1, 0, 1, 2,
+					1, 0, 1, 2,
+					1, 0, 1, 1
+				);
+				mtSegment(
+					hMemDC, t, 1, -3.5,
+					1, 0, 1, 2,
+					2, 0, 2, 2,
+					2, 1, 2, 2
+				);
+				mtSegment(
+					hMemDC, t, 1, -3.5,
+					0, 2, 1, 2,
+					0, 2, 1, 2,
+					0, 1, 1, 1
+				);
+				mtSegment(
+					hMemDC, t, 1, -3.5,
+					1, 2, 2, 2,
+					1, 0, 2, 0,
+					1, 1, 2, 1
+				);
+			}
+			/*  _G_  */ {
+				mtSegment(
+					hMemDC, t, 2, -3.5,
+					2, 0, 2, 0,
+					2, 2, 0, 2,
+					2, 0, 0, 0
+				);
+				mtSegment(
+					hMemDC, t, 2, -3.5,
+					0, 0, 2, 0,
+					0, 0, 0, 2,
+					0, 0, 0, 0
+				);
+				mtSegment(
+					hMemDC, t, 2, -3.5,
+					0, 0, 2, 0,
+					0, 0, 2, 0,
+					0, 0, 2, 0
+				);
+				mtSegment(
+					hMemDC, t, 2, -3.5,
+					1, 0, 2, 0,
+					2, 1, 2, 0,
+					2, 0, 2, 0
+				);
+				mtSegment(
+					hMemDC, t, 2, -3.5,
+					0, 0, 1, 0,
+					1, 1, 2, 1,
+					0, 0, 1, 0
+				);
+			}
+			/*  _S_  */ {
+				mtSegment(
+					hMemDC, t, 3, -3.5,
+					0, 0, 0, 0,
+					0, 2, 2, 2,
+					0, 0, 2, 0
+				);
+				mtSegment(
+					hMemDC, t, 3, -3.5,
+					0, 0, 2, 0,
+					0, 1, 2, 1,
+					1, 0, 1, 0
+				);
+				mtSegment(
+					hMemDC, t, 3, -3.5,
+					2, 0, 2, 0,
+					0, 0, 2, 0,
+					0, 0, 2, 0
+				);
+				mtSegment(
+					hMemDC, t, 3, -3.5,
+					0, 0, 0, 0,
+					0, 1, 0, 2,
+					1, 0, 1, 0
+				);
+				mtSegment(
+					hMemDC, t, 3, -3.5,
+					2, 0, 2, 0,
+					2, 1, 2, 0,
+					1, 0, 1, 0
+				);
+			}
+		}
 
 		DeleteObject(SelectObject(hMemDC, GetStockObject(NULL_PEN)));
 		break;
@@ -158,7 +405,7 @@ void DrawGraph(HDC hdc, HDC hMemDC)
 		for (X = 0; X <= pane.rWidth; X++) {
 			x = (1.0 * (X - pane.paddingX) / pane.radius / 2 - graph.x0) / graph.scale;
 			y = x * x / 4;
-			Y = (graph.y0 - y * graph.scale) * pane.radius * 2;
+			Y = (INT)((graph.y0 - y * graph.scale) * pane.radius * 2);
 			if (X == 0) MoveToEx(hMemDC, pane.lWidth + X, pane.paddingY + Y, NULL);
 			else LineTo(hMemDC, pane.lWidth + X, pane.paddingY + Y);
 		}
@@ -180,9 +427,9 @@ void DrawExpression(HDC hdc, HDC hMemDC)
 
 	// êîéÆÇï`âÊÇ∑ÇÈ
 	SetTextAlign(hMemDC, TA_LEFT | TA_BOTTOM);
-	DeleteObject(SetFont(hMemDC, 20, 0x00FFFF, NULL));
+	DeleteObject(SetFont(hMemDC, 20, 0x00FFFF, 0));
 	TextOut(hMemDC, pane.lWidth + 10, 30, graph.ex, lstrlen(graph.ex));
 
-	DeleteObject(SelectObject(hMemDC, SYSTEM_FONT));
+	DeleteObject(SelectObject(hMemDC, GetStockObject(SYSTEM_FONT)));
 	DeleteObject(SelectObject(hMemDC, GetStockObject(NULL_PEN)));
 }
